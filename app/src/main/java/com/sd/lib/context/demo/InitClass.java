5fc9e61a06e62d120a1996a89c65f54a44960196
zhengjun <565061763@qq.com>
2019-05-30 21:03:46 +0800

    lib_context_init_class

---
+++ app/src/main/java/com/sd/lib/context/demo/InitClass.java
@@ -0,0 +1,11 @@
+package com.sd.lib.context.demo;
+
+import android.util.Log;
+
+public class InitClass
+{
+    public InitClass()
+    {
+        Log.i(InitClass.class.getSimpleName(), "InitClass created");
+    }
+}
--- lib/src/main/java/com/sd/lib/context/FContext.java
+++ lib/src/main/java/com/sd/lib/context/FContext.java
@@ -13,7 +13,25 @@ public class FContext
     static final void set(Context context)
     {
         if (sContext == null)
+        {
             sContext = context.getApplicationContext();
+            init(sContext);
+        }
+    }
+
+    private static void init(Context context)
+    {
+        final String initClass = context.getResources().getString(R.string.lib_context_init_class);
+        if (initClass != null && initClass.length() > 0)
+        {
+            try
+            {
+                Class.forName(initClass).newInstance();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static final Context get()
