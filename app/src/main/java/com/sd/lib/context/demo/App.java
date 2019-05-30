c9ca88886a470c890179aaaf2152cb6f30e55557
zhengjun <565061763@qq.com>
2019-05-30 20:18:33 +0800

    first commit

---
+++ app/src/main/java/com/sd/lib/context/demo/App.java
@@ -0,0 +1,12 @@
+package com.sd.lib.context.demo;
+
+import android.app.Application;
+
+public class App extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
