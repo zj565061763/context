41030acdd1691640efd84a8f46466456a045bf44
zhengjun <565061763@qq.com>
2019-05-30 20:22:59 +0800

    1.0.0-rc1

--- app/src/main/java/com/sd/lib/context/demo/MainActivity.java
+++ app/src/main/java/com/sd/lib/context/demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.sd.lib.context.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.sd.lib.context.FContext;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String appName = FContext.get().getResources().getString(R.string.app_name);
+        Toast.makeText(this, "appName: " + appName, Toast.LENGTH_SHORT).show();
     }
 }
