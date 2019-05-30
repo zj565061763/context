b39f436ab2fb4b9ae987969a7ef55f5967021431
zhengjun <565061763@qq.com>
2019-05-30 21:19:19 +0800

    1.0.1

--- lib/src/main/java/com/sd/lib/context/FContext.java
+++ lib/src/main/java/com/sd/lib/context/FContext.java
@@ -12,21 +12,18 @@ public class FContext
 
     static final void set(Context context)
     {
-        if (sContext == null)
-        {
-            sContext = context.getApplicationContext();
-            init(sContext);
-        }
+        sContext = context.getApplicationContext();
+        init(sContext);
     }
 
     private static void init(Context context)
     {
-        final String initClass = context.getResources().getString(R.string.lib_context_init_class);
-        if (initClass != null && initClass.length() > 0)
+        final String className = context.getResources().getString(R.string.lib_context_init_class);
+        if (className != null && className.length() > 0)
         {
             try
             {
-                Class.forName(initClass).newInstance();
+                Class.forName(className).newInstance();
             } catch (Exception e)
             {
                 e.printStackTrace();
